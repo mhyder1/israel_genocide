339798340dd8ea2ef772b67abe491052bee70437
unknown <hyder1@gmail.com>
2024-02-11 14:55:12 -0500

    added Card to Claims

--- src/components/Claims.jsx
+++ src/components/Claims.jsx
@@ -1,25 +1,12 @@
 import data from "../data";
+import Card from "./Card";
 export default function Claims() {
   const pages = data.slice(111, 112);
   return (
     <>
       <h1>The claims of South Africa</h1>
       {pages.map((item) => (
-        <>
-          <div class="card">
-            <div class="card-body">
-              <p>{item.page}</p>
-              <p>{item.content}</p>
-            </div>
-          </div>
-          {item.subtext && (
-            <ul class="list-group">
-              {item.subtext.map((text) => (
-                <li class="list-group-item">{text}</li>
-              ))}
-            </ul>
-          )}
-        </>
+        <Card data={item} />
       ))}
     </>
   );
