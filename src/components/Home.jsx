282107f143cbddaf8ca5853cc011c018a1895032
unknown <hyder1@gmail.com>
2024-03-16 11:29:39 -0400

    home page wording

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,15 @@
 import { Link } from "react-router-dom";
 import Nav from "./Nav";
 
-
 export default function Home() {
-
-  const style = {
-    display: "flex",
-  };
   return (
     <div class="home">
-      {/* <h1>
-        Better understand the International Court of Justice Crime of Genocide
-        in the Gaza Strip
-      </h1> */}
-      <Nav />
-      {/* <section class="button-menu">
+      <h1>
+        South Africa's Case at the International Court of Justice Against Israel
+        for Committing Genocide in the Gaza Strip
+      </h1>
+      {/* <Nav /> */}
+      <section class="button-menu">
         <Link to="/introduction">
           <button type="button" class="btn btn-secondary btn-lg">
             Introduction
@@ -30,11 +25,6 @@ export default function Home() {
             The Facts
           </button>
         </Link>
-        <Link to="/facts/genocidal_acts">
-          <button type="button" class="btn btn-secondary btn-lg">
-            Genocidal Acts
-          </button>
-        </Link>
         <Link to="/claims">
           <button type="button" class="btn btn-secondary btn-lg">
             Claims of South Africa
@@ -44,23 +34,8 @@ export default function Home() {
           <button type="button" class="btn btn-secondary btn-lg">
             Search
           </button>
-        </Link> 
-      </section>*/}
-      {/* <h3>APPLICATION INSTITUTING PROCEEDINGS</h3>
-      <p>
-        To the Registrar of the International Court of Justice, the undersigned,
-        being duly authorised by the Government of the Republic of South Africa,
-        state as follows:
-      </p>
-      <p>
-        In accordance with Articles 36 (1) and 40 of the Statute of the Court
-        and Article 38 of the Rules of Court, I have the honour to submit this
-        Application instituting proceedings in the name of the Republic of South
-        Africa (“South Africa”) against the State of Israel (“Israel”). Pursuant
-        to Article 41 of the Statute, the Application includes a request that
-        the Court indicate provisional measures to protect the rights invoked
-        herein from imminent and irreparable loss.
-      </p> */}
+        </Link>
+      </section>
     </div>
   );
 }
